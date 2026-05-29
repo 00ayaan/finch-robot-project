bdffd4d507370f4cc66a768bc28689ab7bb0c42d
00ayaan <ayaanarneja@gmail.com>
2026-05-29 14:18:47 -0700

    v3

--- src/main/java/com/heyisaac/TesterFinch.java
+++ src/main/java/com/heyisaac/TesterFinch.java
@@ -4,21 +4,67 @@ public class TesterFinch{
     public static void main(String[] args) {
         FinalFinch bird = new FinalFinch("A");
 
+
+
+//     for (int i = 0; i<2; i++){
+
+//         while (bird.getDistance() > 10){
+//             bird.setMove("F", 5, 100);
+//         }
+
+//         bird.setTurn("L", 90, 50);
+
+//         while (bird.getDistance() > 8){
+//             bird.setMove("F", 5, 100);
+//         }
+
+//         bird.setTurn("L", 90, 50);
+
+//         while (bird.getDistance() > 10){
+//             bird.setMove("F", 5, 100);
+//         }
+
+//         bird.setTurn("R", 90, 50);
+
+//         while (bird.getDistance() > 8){
+//             bird.setMove("F", 5, 100);
+//         }
+
+//         bird.setTurn("R", 90, 50);
+
+// }
+
         while (bird.getDistance() > 10){
+            bird.setMove("F", 5, 100);
+
+        }
+
+        bird.setTurn("R", 100, 50);
+
+
+        while (bird.getDistance() > 8){
             bird.setMove("F", 5, 100);
         }
 
         bird.setTurn("L", 90, 50);
 
-        while (bird.getDistance() > 10){
+        while (bird.getDistance() > 8){
             bird.setMove("F", 5, 100);
         }
-
 
         bird.setTurn("L", 90, 50);
 
-        while (bird.getDistance() > 10){
+        
+        while (bird.getDistance() > 8){
             bird.setMove("F", 5, 100);
         }
+
+        bird.setTurn("R", 90, 50);
+
+
+
+
+
+
     }
-}
+} 
